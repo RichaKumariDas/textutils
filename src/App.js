e6bd4c994e4bd110e52a329552a00a4245924c9b
RichaKumariDas <richadas9801@gmail.com>
2025-06-15 18:37:21 +0530

    Updated Navbar, TextForm, and added HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import React, { useState } from "react";
-import Alert from "./components/Alert";
-import TextForm from "./components/TextForm";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';  // important change
 
 function App() {
-  const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type,
+      type: type
     });
     setTimeout(() => {
       setAlert(null);
@@ -20,29 +19,29 @@ function App() {
   };
 
   const toggleMode = () => {
-    if (mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "#042743";
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-      document.title = "TextUtils - Dark Mode";
     } else {
-      setMode("light");
-      document.body.style.backgroundColor = "white";
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title = "TextUtils - Light Mode";
     }
   };
 
   return (
-    <Router>
-      <Navbar title="Textutils" about="About Textutils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <Routes>
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <Navbar title="Textutils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/" element={<TextForm showAlert={showAlert} heading="Try Textutils - word counter, character counter, remove extra spaces" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
